326854d2daf3e49885bbd4352d9fd9c57ab79d2c
Afrid <mahmidamanha@gmail.com>
2025-07-09 11:52:02 +0600

    adding js file

---
+++ script.js
@@ -0,0 +1,23 @@
+<script>
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+      const targetId = this.getAttribute('href');
+      const targetElement = document.querySelector(targetId);
+      
+      if (targetElement) {
+        window.scrollTo({
+          top: targetElement.offsetTop - 80, // Adjust for fixed header height
+          behavior: 'smooth'
+        });
+        
+        // Update URL without jumping
+        if (history.pushState) {
+          history.pushState(null, null, targetId);
+        } else {
+          window.location.hash = targetId;
+        }
+      }
+    });
+  });
+</script>
